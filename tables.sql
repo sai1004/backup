a93f94909ef80d893a3dcd86dc16f50a8b06bea0
dff-staff <dff-staff@localhost.localdomain>
2019-03-11 20:10:51 +0530

    commit

--- tables.sql
+++ tables.sql
@@ -1,39 +1,39 @@
-To create a database
+-- To create a database
 
-CREATE DATABASE mydb;
-========================================
-To use created db
+-- CREATE DATABASE mydb;
+-- ========================================
+-- To use created db
 
-USE mydb;
-===============+++===========
-To create a table in db
+-- USE mydb;
+-- ===============+++===========
+-- To create a table in db
 
-CREATE TABLE profile(
-        Id varchar(99) primary key, 
-        name varchar(50) not null, 
-        email varchar(50) not null, 
-        password varchar(50) not null
-);
+-- CREATE TABLE profile(
+--         Id varchar(99) primary key, 
+--         name varchar(50) not null, 
+--         email varchar(50) not null, 
+--         password varchar(50) not null
+-- );
 
-========================================
-to show databases
+-- ========================================
+-- to show databases
 
-SHOW DATABASES;
-========================================
+-- SHOW DATABASES;
+-- ========================================
 
-To insert a value in row of a table
+-- To insert a value in row of a table
 
-INSERT INTO profile ( ID, name, email, password)
-value ( '12', 'sai', sai@gmail.com'', 'pass')
+-- INSERT INTO profile ( ID, name, email, password)
+-- value ( '12', 'sai', sai@gmail.com'', 'pass')
 
-========================================
+-- ========================================
 
-To get the specific details by conditions
+-- To get the specific details by conditions
 
-SELECT * FROM profile WHERE Id = '12'
+-- SELECT * FROM profile WHERE Id = '12'
 
-Output will be
-Id    name      email                 password 
-12   sai    sai@gmail.com       pass
+-- Output will be
+-- Id    name      email                 password 
+-- 12   sai    sai@gmail.com       pass
 
-========================================
+-- ========================================
